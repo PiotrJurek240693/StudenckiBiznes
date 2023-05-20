af674e6d07865cc06c697ceaed11c137089abdd2
Piotr Jurek <240693@edu.p.lodz.pl>
2023-05-20 22:58:11 +0200

    Napisanie podstawowych metod do obsugi serwera

--- src/connection/client/Client.java
+++ src/connection/client/Client.java
@@ -1,2 +1,4 @@
-package connection.client;public class Client {
+package connection.client;
+
+public class Client {
 }
--- src/connection/server/Server.java
+++ src/connection/server/Server.java
@@ -1,2 +1,51 @@
-package connection.server;public class Server {
+package connection.server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class Server extends Thread {
+    private final ArrayList<ClientHandler> clientHandlers;
+    private final ServerSocket serverSocket;
+    private boolean working;
+
+    Server() throws IOException {
+        serverSocket = new ServerSocket(8888);
+        clientHandlers = new ArrayList<>();
+        working = true;
+        System.out.println("Serwer nasłuchuje na porcie 8888.");
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (working) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Nowe połączenie: " + clientSocket);
+
+                ClientHandler clientHandler = new ClientHandler(clientSocket);
+                clientHandler.start();
+                clientHandlers.add(clientHandler);
+            }
+        } catch (IOException e) {
+            System.out.println("Bład podczas dołączania nowego klienta!");
+            close();
+        }
+        System.out.println("Serwer zakończył działanie.");
+    }
+
+    public void sendMessage(String message) {
+        for (ClientHandler ch : clientHandlers) {
+            ch.sendMessage(message);
+        }
+    }
+
+    public void close() {
+        for (ClientHandler ch : clientHandlers) {
+            ch.close();
+        }
+        working = false;
+        this.interrupt();
+    }
 }
