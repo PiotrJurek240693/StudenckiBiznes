e3bf001e5780494bef894d7ff28d5fec416507a3
Naruumi02 <240696@edu.p.lodz.pl>
2023-05-28 19:27:37 +0200

    Dodanie klasy stackOfCards oraz jej podstawowych metod

--- src/gameLogic/GameInfo.java
+++ src/gameLogic/GameInfo.java
@@ -5,4 +5,5 @@ public class GameInfo
     public static final int START_SQUARE_ADDITION = 200;
     public static final int START_VALUE = 1600;
     public static final int NUMBER_OF_SQUARES = 40;
+    public static final int SUB_INDEX = -1;
 }
