1422297650c56323c5be361b7d039fa6b2331125
Naruumi02 <240696@edu.p.lodz.pl>
2023-05-28 15:12:26 +0200

    Dodanie getterow i setterow w klasie Player

--- src/gameLogic/Pawn.java
+++ src/gameLogic/Pawn.java
@@ -2,8 +2,9 @@ package gameLogic;
 
 public class Pawn
 {
-    private int position;
+    private int position = 0;
 
+    public int getPosition(){return position;}
     public int move(int shift)
     {
         position += shift;
--- src/gameLogic/Player.java
+++ src/gameLogic/Player.java
@@ -1,13 +1,32 @@
 package gameLogic;
 
 public class Player {
-    private int moneyAmount = 0;
-    private Pawn pawn;
-    // TODO: (gameLogic) zrobić klasę Player. Aktualną można usunąć lub przebudować
+    private int moneyAmount = 1600;
+    private int inDante = 0;
+    private boolean isBankrupt = false;
+    private final Pawn pawn = new Pawn();
+
+
+
     public void setPosition(int shift)
     {
         moneyAmount += pawn.move(shift);
     }
     public int getDices(){return 2;}
-    public int getPosition(){return 0;}
+    public int getPosition(){return pawn.getPosition();}
+    public int getInDante()
+    {
+        return inDante;
+    }
+    public boolean isBankrupt()
+    {
+        return isBankrupt;
+    }
+    public void setInDante(int numberOfRounds)
+    {
+        inDante = numberOfRounds;
+    }
+    public void setInBankrupt(){isBankrupt = true;}
+
+
 }
