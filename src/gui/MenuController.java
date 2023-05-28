c1e70a414e0c03389123dd83ab166b3e28932327
Piotr Jurek <240693@edu.p.lodz.pl>
2023-05-28 22:17:12 +0200

    Dodanie kolejnej czesci menu

--- src/gui/MenuController.java
+++ src/gui/MenuController.java
@@ -1,6 +1,9 @@
 package gui;
 
+import connection.server.Server;
 import javafx.application.Platform;
+
+import java.io.IOException;
 
 public class MenuController {
     public static void onExitButtonClick() {
@@ -13,5 +16,13 @@ public class MenuController {
 
     public static void onReturnToMainMenuButtonClick() {
         MenuShower.showMainMenu();
+    }
+
+    public static void onMultiplayerButtonClick() {
+        MenuShower.showHostOrJoinMenu();
+    }
+
+    public static void onReturnToMultiOrSingleMenuButtonClick() {
+        MenuShower.showMultiOrSingleMenu();
     }
 }
--- src/gui/MenuShower.java
+++ src/gui/MenuShower.java
@@ -9,7 +9,6 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 
 public class MenuShower {
-
     public static void showMainMenu() {
         // Tworzenie obiektu MenuPowitlane
         ImageView MenuPowitalne = new ImageView();
@@ -81,6 +80,7 @@ public class MenuShower {
         button2.getStyleClass().add("przyciski_menu_powitalne");
         button2.setTranslateX(20);
         button2.setTranslateY(130);
+        button2.setOnAction(event -> {MenuController.onMultiplayerButtonClick();});
 
         // Tworzenie przycisku 3
         Button button3 = new Button("Powrót");
@@ -88,6 +88,53 @@ public class MenuShower {
         button3.setTranslateX(20);
         button3.setTranslateY(150);
         button3.setOnAction(event -> {MenuController.onReturnToMainMenuButtonClick();});
+
+        // Tworzenie kontenera VBox i ustawianie przycisków na środku
+        VBox buttonContainer = new VBox(10, button1, button2, button3);
+        buttonContainer.setAlignment(Pos.CENTER);
+
+        // Tworzenie kontenera StackPane i dodawanie MenuPowitalne oraz przycisków do niego
+        StackPane root = new StackPane();
+        root.getChildren().addAll(MenuPowitalne, buttonContainer);
+
+        // Tworzenie sceny i ustawianie jej w Stage
+        Scene scene = new Scene(root, MenuPowitlaneImage.getWidth(), MenuPowitlaneImage.getHeight());
+        scene.getStylesheets().add(MenuShower.class.getResource("styles.css").toExternalForm()); // Dodanie arkusza stylów CSS
+
+        ScreenSettings.primaryStage.setScene(scene);
+        ScreenSettings.primaryStage.show();
+    }
+
+    public static void showHostOrJoinMenu() {
+        // Tworzenie obiektu MenuPowitlane
+        ImageView MenuPowitalne = new ImageView();
+
+        // Wczytywanie obrazu
+        Image MenuPowitlaneImage = new Image("file:../../assets/MenuPowitalne.png");
+
+        // Ustawianie wczytanego obrazu z MenuPowitalne
+        MenuPowitalne.setImage(MenuPowitlaneImage);
+        MenuPowitalne.setFitWidth(ScreenSettings.screenWidth);
+        MenuPowitalne.setFitHeight(ScreenSettings.screenHeight);
+
+        // Tworzenie przycisku 1
+        Button button1 = new Button("Hostuj");
+        button1.getStyleClass().add("przyciski_menu_powitalne");
+        button1.setTranslateX(20);
+        button1.setTranslateY(110);
+
+        // Tworzenie przycisku 2
+        Button button2 = new Button("Dołącz");
+        button2.getStyleClass().add("przyciski_menu_powitalne");
+        button2.setTranslateX(20);
+        button2.setTranslateY(130);
+
+        // Tworzenie przycisku 3
+        Button button3 = new Button("Powrót");
+        button3.getStyleClass().add("przyciski_menu_powitalne");
+        button3.setTranslateX(20);
+        button3.setTranslateY(150);
+        button3.setOnAction(event -> {MenuController.onReturnToMultiOrSingleMenuButtonClick();});
 
         // Tworzenie kontenera VBox i ustawianie przycisków na środku
         VBox buttonContainer = new VBox(10, button1, button2, button3);
