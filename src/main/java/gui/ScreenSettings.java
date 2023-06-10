f847d9a2ef9417cf2aaf12b3fa0906313d752a1a
Emil Sarba <240793@edu.p.lodz.pl>
2023-06-10 11:12:43 +0200

    ustawienia do skalowania obrazu

--- src/main/java/gui/ScreenSettings.java
+++ src/main/java/gui/ScreenSettings.java
@@ -1,5 +1,6 @@
 package gui;
 
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class ScreenSettings {
@@ -7,11 +8,23 @@ public class ScreenSettings {
     public static int screenWidth = 1920;
     public static int screenHeight = 1080;
     public static boolean isFullScreen = false;
+    public static double scale=1;
 
+    public static void changeScale(double scaleChange)
+    {
+        scale+=scaleChange;
+        if(scale>1)scale=1;
+        if(scale<0.2)scale=0.2;
+    }
     public static void initializeScreen(){
         primaryStage.setResizable(false);
         primaryStage.setFullScreen(isFullScreen);
         primaryStage.setWidth(screenWidth);
         primaryStage.setHeight(screenHeight);
+    }
+    public static void resize(Scene scene)
+    {
+        scene.getRoot().setScaleX(scale);
+        scene.getRoot().setScaleY(scale);
     }
 }
